3ab932e6918917fcfd105df6a8769dace195c351
Pim van der Loos <pimvanderloos@gmail.com>
2022-07-14 00:24:17 +0200

    Set stamina bar before sending to player
    - Fixed an issue where players receiving a stamina bar for exhausted horses would see an initial flicker of 100% stamina before seeing the actual value on the next update. The issue was caused by adding the player to the bossbar before setting the correct values.

--- src/main/java/nl/pim16aap2/horses/staminabar/StaminaNotifierManager.java
+++ src/main/java/nl/pim16aap2/horses/staminabar/StaminaNotifierManager.java
@@ -10,13 +10,13 @@ public class StaminaNotifierManager
 {
     private final Map<Player, IStaminaNotifier> notifierMap = new HashMap<>();
 
-    public @Nullable IStaminaNotifier getNewNotifier(Player player)
+    public @Nullable IStaminaNotifier getNewNotifier(Player player, double staminaPercentage, boolean exhausted)
     {
         //noinspection SpellCheckingInspection
         if (!player.hasPermission("horses.user.staminabar"))
             return null;
 
-        final IStaminaNotifier notifier = new StaminaBar(player);
+        final IStaminaNotifier notifier = new StaminaBar(player, staminaPercentage, exhausted);
         final @Nullable IStaminaNotifier oldNotifier = notifierMap.put(player, notifier);
         if (oldNotifier != null)
             oldNotifier.kill();
