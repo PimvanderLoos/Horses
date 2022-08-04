82e8b0cb5d3e0e66a45c6d984d254cd32eec9ec9
Pim van der Loos <pimvanderloos@gmail.com>
2022-08-04 15:00:07 +0200

    Include admin variants when checking permissions

--- src/main/java/nl/pim16aap2/horses/util/Permission.java
+++ src/main/java/nl/pim16aap2/horses/util/Permission.java
@@ -1,20 +1,35 @@
 package nl.pim16aap2.horses.util;
 
 import org.bukkit.entity.Player;
+import org.jetbrains.annotations.Nullable;
 
+@SuppressWarnings("unused")
 public enum Permission
 {
-    USER_SEE_STAMINA_BAR("horses.user.see_stamina_bar"),
-    USER_MAKE_GELDING("horses.admin.make_gelding"),
     ADMIN_FEED_BABY("horses.admin.feed_baby"),
     ADMIN_MAKE_GELDING("horses.admin.make_gelding"),
+
+    USER_SEE_STAMINA_BAR("horses.user.see_stamina_bar"),
+    USER_MAKE_GELDING(ADMIN_MAKE_GELDING, "horses.admin.make_gelding"),
     ;
 
+    private final @Nullable Permission adminVariant;
     private final String node;
+
+    Permission(@Nullable Permission adminVariant, String node)
+    {
+        this.node = node;
+        this.adminVariant = adminVariant;
+    }
 
     Permission(String node)
     {
-        this.node = node;
+        this(null, node);
+    }
+
+    public @Nullable Permission getAdminVariant()
+    {
+        return adminVariant;
     }
 
     public String getNode()
@@ -24,6 +39,7 @@ public enum Permission
 
     public boolean isSetFor(Player player)
     {
-        return player.hasPermission(node);
+        return player.hasPermission(node) ||
+            (adminVariant != null && adminVariant.isSetFor(player));
     }
 }
